ad1c0692d553257e601f789493a8acd1cd8f4617
karncharuenrit <karnjaruenrit@gmail.com>
2019-10-22 15:49:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<?php
-echo "i am bot";
-
-
 curl -X POST \
 -H 'Content-Type:application/json' \
 -H 'Authorization: Bearer XKj2ZnsCmYOyRuZwgCCkP4J37Dd+D97qv4aajOWUkTeh9erjOmYC4hXSwiI1mJTuaQ0Z0B2ZOQHHW4jMYWifptIb29Gew62KWD/8oMSN+eGPcgBuVz3f76z+kQIzAqu5BtSJ/gRnNuZUu3IuT2CqowdB04t89/1O/w1cDnyilFU=' \
+
+<?php
+echo "i am botA";
+
+
+
