fd90d4aef46c60d265289982214f909c5da8977a
karncharuenrit <karnjaruenrit@gmail.com>
2019-10-24 14:11:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -58,37 +58,37 @@ function sentMessage($encodeJson,$datas)
     return $datasReturn;
 }
 
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-}
+// $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// try {
+//   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+//   error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+//   error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+//   error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+//   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+// }
 
-foreach($events as $event){
-    if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
-        $logger->info('Postback message has come');
-        continue;
-      }
-      // Location Event
-      if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-        $logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
-        continue;
-      }
+// foreach($events as $event){
+//     if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
+//         $logger->info('Postback message has come');
+//         continue;
+//       }
+//       // Location Event
+//       if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
+//         $logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
+//         continue;
+//       }
       
-      // Message Event = TextMessage
-      if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-        // get message text
-        $messageText=strtolower(trim($event->getText()));
+//       // Message Event = TextMessage
+//       if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+//         // get message text
+//         $messageText=strtolower(trim($event->getText()));
         
-      }
-}
+//       }
+// }
 
 $messages = [];
 $messages['replyToken'] = $replyToken;
