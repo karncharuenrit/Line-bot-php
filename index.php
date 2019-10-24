6c35f562e8e475666345840bfd3d83f479706bdf
karncharuenrit <karnjaruenrit@gmail.com>
2019-10-24 09:18:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,68 @@
 <?php
 /*Return HTTP Request 200*/
+//ทำการ Return Response Status 200 กลับไปให้ LINE ก่อน เพื่อตรวจสอบว่า LINE Webhook สามารถเชื่อมมายัง Server เราได้
 http_response_code(200);
 
-
+file_put_contents('log.txt',file_get_contents('php://input').PHP_EOL,FILE_APPEND);//เราใช้เพื่ออ่านข้อมูล Data ที่ LINE ส่งเข้ามาซึ่งเราจะได้ข้อมูลในลักษณะของ Json แบบนี้
 echo "i am bot A";
-// require_once '/Users/kan/Line-bot-php/index.php';
 
-// use Monolog\Logger;
-// use Monolog\Handler\StreamHandler;
-// use Monolog\Handler\FirePHPHandler;
+$datas = file_get_contents('php://input'); //$datas : เราสร้างตัวแปรนี้ขึ้นมาเพื่อไว้สำหรับเก็บ Datas ที่เราได้รับมาจาก LINE
+$deCode = json_decode($datas,true); //$decode : เก็บค่า Array หลังจาก Decode แล้วโดยใช้คำสั่ง json_decode
 
-// $logger = new Logger('LineBot');
-// $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["hV49GKQw+K2jv0VCyJ2BT6tYiQm6dwweGBtDCW/TrudXBXzju8p0rojagOepJgAXaQ0Z0B2ZOQHHW4jMYWifptIb29Gew62KWD/8oMSN+eHFgyoZ9trsFeI06j2YId2mSxEcnypVdsUn0fz3GP5uIQdB04t89/1O/w1cDnyilFU="]);
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["0b524fb063d9b92c9c7debd29e5bbae0"]]);
+$replyToken = $deCode['events'][0]['replyToken']; //เก็บข้อมูล replytoken ซึ่ง replytoken นี้เอาไว้สำหรับใช้ในการตอบข้อความแบบ reply (ตอบกลับทันทีหลังจากที่มีการส่ง Datas จาก LINE เข้ามา)
 
-// $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-// try {
-//   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-// } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-//   error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-//   error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-//   error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-//   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-// }
-// foreach ($events as $event) {
-//   // Postback Event
-//   if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
-//     $logger->info('Postback message has come');
-//     continue;
-//   }
-//   // Location Event
-//   if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-//     $logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
-//     continue;
-//   }
-  
-//   // Message Event = TextMessage
-//   if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-//     // get message text
-//     $messageText=strtolower(trim($event->getText()));
-    
-//   }
-// }  
+function getFormatTextMessage($text)
+{
+   $datas = [];
+   $datas['type'] = 'text';
+   $datas['text'] = $text;
+   return $datas;
+}
+
+function sentMessage($encodeJson,$datas)
+{
+    $datasReturn = [];
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+          CURLOPT_URL => $datas['url'],
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => $encodeJson,
+          CURLOPT_HTTPHEADER => array(
+            "authorization: Bearer ".$datas['token'],
+            "cache-control: no-cache",
+            "content-type: application/json; charset=UTF-8",
+          ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+        $datasReturn['result'] = 'E';
+        $datasReturn['message'] = $err;
+    } else {
+        if($response == "{}"){
+           $datasReturn['result'] = 'S';
+           $datasReturn['message'] = 'Success';
+        }else{
+           $datasReturn['result'] = 'E';
+           $datasReturn['message'] = $response;
+        }
+    }
+    return $datasReturn;
+}
+
+$messages = [];
+$messages['replyToken'] = $replyToken;
+$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+$encodeJson = json_encode($messages);
+$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+$LINEDatas['token'] = "hV49GKQw+K2jv0VCyJ2BT6tYiQm6dwweGBtDCW/TrudXBXzju8p0rojagOepJgAXaQ0Z0B2ZOQHHW4jMYWifptIb29Gew62KWD/8oMSN+eHFgyoZ9trsFeI06j2YId2mSxEcnypVdsUn0fz3GP5uIQdB04t89/1O/w1cDnyilFU=";
+$results = sentMessage($encodeJson,$LINEDatas);
+
+
 ?>
