b82c0109c2a124890072ed6236c3fedd1981d5f3
karncharuenrit <karnjaruenrit@gmail.com>
2019-10-25 10:04:25 +0700

    fff

---
+++ firebase.php
@@ -0,0 +1,31 @@
+<html>
+
+<header>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="/__/firebase/7.2.2/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="/__/firebase/7.2.2/firebase-analytics.js"></script>
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
+    <script>
+        const firebaseConfig = {
+            apiKey: "AIzaSyBKnAggcRfmdDSGlTorWDZlYLvNGj4TSqQ",
+            authDomain: "readsid-5a802.firebaseapp.com",
+            databaseURL: "https://readsid-5a802.firebaseio.com",
+            projectId: "readsid-5a802",
+            storageBucket: "readsid-5a802.appspot.com",
+            messagingSenderId: "722743443476",
+            appId: "1:722743443476:web:b9e4efb51297b90103a701",
+            measurementId: "G-4FTMTBWCQR"
+        };
+    </script>
+</header>
+
+<body>
+
+</body>
+
+</html>
