a7845dfd890f0aee8b1538f2afd4d7bdfbe0c41b
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-04 16:57:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -120,14 +120,15 @@ if (sizeof($request_array['events']) > 0) {
         if ($event['type'] == 'message') {
             if ($event['message']['type'] == 'text') {
                 $text = $event['message']['text'];
+                echo '' . $text . '';
                 $reply_message = '' . $text . '';
-                
             } else
-                $reply_message = '' . $event['message']['type'] . '';
-                
+                echo '' . $event['message']['type'] . '';
+            $reply_message = '' . $event['message']['type'] . '';
         } else
-            $reply_message = '' . $event['type'] . '';
-            
+            echo '' . $event['type'] . '';
+        $reply_message = '' . $event['type'] . '';
+
 
         if (strlen($reply_message) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
