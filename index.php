3de1c612abcbc8afaa3f2af2f02c753702ced7da
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-04 12:54:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -83,6 +83,7 @@ function sentMessage($encodeJson, $datas)
             "content-type: application/json; charset=UTF-8",
 
         ),
+        
 
     ));
     $response = curl_exec($curl);
@@ -103,6 +104,68 @@ function sentMessage($encodeJson, $datas)
     return $datasReturn;
 }
 
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = 'hV49GKQw+K2jv0VCyJ2BT6tYiQm6dwweGBtDCW/TrudXBXzju8p0rojagOepJgAXaQ0Z0B2ZOQHHW4jMYWifptIb29Gew62KWD/8oMSN+eHFgyoZ9trsFeI06j2YId2mSxEcnypVdsUn0fz3GP5uIQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+if ( sizeof($request_array['events']) > 0 )
+{
+
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+
+  if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
+}
+
+echo "OK";
+
+function send_reply_message($url, $post_header, $post_body)
+{
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+
+ return $result;
+}
+
+?>
+
 // $message = $arrayJson['events'][0]['message']['text']; //รับข้อความจากผู้ใช้
 // $id = $arrayJson['events'][0]['source']['userId']; //recive id form user 
 $reply_messages = '';
@@ -111,11 +174,6 @@ $messages['replyToken'] = $replyToken;
 $userID = $events['events'][0]['source']['userId'];
 $sourceType = $events['events'][0]['source']['type'];
 //$text = $messaage['mmessage']['type']['text'];
-$request = file_get_contents('https://ipjastelbottest.herokuapp.com/');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-$request_array['events'] = $event;
-$event['type'] = 'message';
-$event['message']['type'] = 'text';
 $text = $event['message']['text'];
 $reply_message = '('.$text.')';
 $messages['messages'][0] = getFormatTextMessage($reply_message);
