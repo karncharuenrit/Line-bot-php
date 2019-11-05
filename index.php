289b5d7801df621054cc86326717b3dbb478e4a8
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-05 10:04:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -131,6 +131,7 @@ if (sizeof($request_array['events']) > 0) {
             $reply_messages = '';
             $messages = [];
             $messages['messages'][0] = getFormatTextMessage(''.$text.'');
+            $encodeJson = json_encode($messages);
             $messages['replyToken'] = $replyToken;
             $LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
             $LINEDatas['token'] = "hV49GKQw+K2jv0VCyJ2BT6tYiQm6dwweGBtDCW/TrudXBXzju8p0rojagOepJgAXaQ0Z0B2ZOQHHW4jMYWifptIb29Gew62KWD/8oMSN+eHFgyoZ9trsFeI06j2YId2mSxEcnypVdsUn0fz3GP5uIQdB04t89/1O/w1cDnyilFU=";
