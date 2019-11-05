a6ffd57a382f16726e730c0cea34ca2b7ab645d2
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-05 14:44:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ if (sizeof($request_array['events']) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
             $img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
             $outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
-
+            $response = $bot->replyMessage($event->getReplyToken(), $outputText);
             $data = [
                 'replyToken' => $reply_token,
                 'messages' => [['type' => 'text', 'text' => $reply_message]]
