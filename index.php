d23711895915cc356a67eeafea87a06ffd672b09
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-05 14:58:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ use LINE\LINEBot\HTTPClient;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
+
 $factory = (new Factory)
     ->withServiceAccount('./secret/readsid-5a802-d428a33cbfdc.json')
     // The following line is optional if the project id in your credentials file
@@ -20,6 +21,9 @@ $factory = (new Factory)
 $database = $factory->createDatabase();
 
 // die(print_r($database));
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+
 
 http_response_code(200);
 
@@ -37,6 +41,7 @@ $arrayJson = json_decode($content, true);
 $arrayHeader = array();
 $arrayHeader[] = "Content-Type: application/json";
 $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
 
 function getFormatTextMessage($text)
 {
@@ -137,9 +142,7 @@ if (sizeof($request_array['events']) > 0) {
 
         if (strlen($reply_message) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
-            $img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-            $outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
-            $response = $bot->replyMessage($event->getReplyToken(), $outputText);
+
             $data = [
                 'replyToken' => $reply_token,
                 'messages' => [['type' => 'text', 'text' => $reply_message]]
@@ -170,9 +173,100 @@ function send_reply_message($url, $post_header, $post_body)
 }
 //--------------------------------------------------imagebuilder------------------------------------------------
 
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+
+$events = json_decode($content, true);
+if (!is_null($events)) {
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    $userMessage = strtolower($userMessage);
+    switch ($typeMessage) {
+        case 'text':
+            switch ($userMessage) {
+                case "t":
+                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
+                case "i":
+                    $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
+                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
+                    $replyData = new ImageMessageBuilder($picFullSize, $picThumbnail);
+                    break;
+                case "v":
+                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
+                    $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";
+                    $replyData = new VideoMessageBuilder($videoUrl, $picThumbnail);
+                    break;
+                case "a":
+                    $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
+                    $replyData = new AudioMessageBuilder($audioUrl, 27000);
+                    break;
+                case "l":
+                    $placeName = "ที่ตั้งร้าน";
+                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+                    $latitude = 13.780401863217657;
+                    $longitude = 100.61141967773438;
+                    $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude, $longitude);
+                    break;
+                case "s":
+                    $stickerID = 22;
+                    $packageID = 2;
+                    $replyData = new StickerMessageBuilder($packageID, $stickerID);
+                    break;
+                case "im":
+                    $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
+                    $replyData = new ImagemapMessageBuilder(
+                        $imageMapUrl,
+                        'This is Title',
+                        new BaseSizeBuilder(699, 1040),
+                        array(
+                            new ImagemapMessageActionBuilder(
+                                'test image map',
+                                new AreaBuilder(0, 0, 520, 699)
+                            ),
+                            new ImagemapUriActionBuilder(
+                                'http://www.ninenik.com',
+                                new AreaBuilder(520, 0, 520, 699)
+                            )
+                        )
+                    );
+                    break;
+                case "tm":
+                    $replyData = new TemplateMessageBuilder(
+                        'Confirm Template',
+                        new ConfirmTemplateBuilder(
+                            'Confirm template builder',
+                            array(
+                                new MessageTemplateActionBuilder(
+                                    'Yes',
+                                    'Text Yes'
+                                ),
+                                new MessageTemplateActionBuilder(
+                                    'No',
+                                    'Text NO'
+                                )
+                            )
+                        )
+                    );
+                    break;
+                default:
+                    $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            $replyData = new TextMessageBuilder($textReplyMessage);
+            break;
+    }
+}
+$textMessageBuilder = new TextMessageBuilder(json_encode($events));
 ?>
 
-<!-- // $message = $arrayJson['events'][0]['message']['text']; //รับข้อความจากผู้ใช้
+        <!-- // $message = $arrayJson['events'][0]['message']['text']; //รับข้อความจากผู้ใช้
 // $id = $arrayJson['events'][0]['source']['userId']; //recive id form user 
 $reply_messages = '';
 $messages = [];
@@ -189,4 +283,4 @@ $LINEDatas['token'] = "hV49GKQw+K2jv0VCyJ2BT6tYiQm6dwweGBtDCW/TrudXBXzju8p0rojag
 $results = sentMessage($encodeJson, $LINEDatas);; -->
 
 
-<!-- //test2 -->
+        <!-- //test2 -->
