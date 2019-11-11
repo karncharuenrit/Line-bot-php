abe3f698cfc54de96e88ab70d223731f87df1480
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-11 13:45:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -219,7 +219,7 @@ function send_reply_message($url, $post_header, $post_body)
 }
 //--------------------------------------------------imagebuilder-----------------------------------------------
 
-function replyMsg($arrayHeader, $arrayJsonLine)
+function replyMsg($arrayHeader, $arrayPostData)
 {
   $strUrl = "https://api.line.me/v2/bot/message/reply";
   $ch = curl_init();
@@ -228,18 +228,13 @@ function replyMsg($arrayHeader, $arrayJsonLine)
   curl_setopt($ch, CURLOPT_DNS_LOCAL_IP4, false);
   curl_setopt($ch, CURLOPT_POST, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayJsonLine));
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   $result = curl_exec($ch);
   curl_close($ch);
 }
 exit;
-
-$flexmessage = file_get_contents('./line.json');
-$arrayJsonLine = json_decode($flexmessage,true);
-
-
 
 
 ?>
