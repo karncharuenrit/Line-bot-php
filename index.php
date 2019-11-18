52a46e0039e8315644fa721a10beb370ddcd6366
karncharuenrit <karnjaruenrit@gmail.com>
2019-11-18 16:19:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -167,6 +167,52 @@ if ($message == "c1553") {
 }
 
 
+$textReplyMessage = new CarouselContainerBuilder(
+  array(
+      new BubbleContainerBuilder(
+          "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
+          NULL,NULL,
+          new BoxComponentBuilder(
+              "horizontal",
+              array(
+                  new TextComponentBuilder("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.",NULL,NULL,NULL,NULL,NULL,true)
+              )
+          ),
+          new BoxComponentBuilder(
+              "horizontal",
+              array(
+                  new ButtonComponentBuilder(
+                      new UriTemplateActionBuilder("GO","http://niik.in"),
+                      NULL,NULL,NULL,"primary"
+                  )
+              )
+          )
+      ), // end bubble 1
+      new BubbleContainerBuilder(
+          "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
+          NULL,NULL,
+          new BoxComponentBuilder(
+              "horizontal",
+              array(
+                  new TextComponentBuilder("Hello, World!",NULL,NULL,NULL,NULL,NULL,true)
+              )
+          ),
+          new BoxComponentBuilder(
+              "horizontal",
+              array(
+                  new ButtonComponentBuilder(
+                      new UriTemplateActionBuilder("GO","http://niik.in"),
+                      NULL,NULL,NULL,"primary"
+                  )
+              )
+          )
+      ) // end bubble 2       
+  )
+);
+$replyData = new FlexMessageBuilder("Flex",$textReplyMessage);
+ 
+
 
 // //if (sizeof($request_array['events']) > 0) {
 
